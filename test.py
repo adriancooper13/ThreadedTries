7bc2106a729bd20c0a3d8ae61e2225c5268e1079
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-03-21 19:03:40 -0400

    script to run different test cases and graph results

---
+++ test.py
@@ -0,0 +1,119 @@
+import matplotlib
+matplotlib.use("agg")
+
+import matplotlib.pyplot as plt
+import numpy as np 
+import os
+import sys
+
+TEST_DIR = "testing/"
+TRIES_DIR = "tries/"
+JSON_FILENAME = "json.txt"
+VERSIONS = ["Optimistic", "WaitFree"]
+
+# Get number of command line arguments (line C).
+argv = sys.argv
+argc = len(argv)
+
+if argc < 2 or not argv[1].isdigit():
+    count = 0
+    max_test = None 
+else:
+    count = int(argv[1]) - 1
+    max_test = count + 1
+
+while True:
+    # Increase count for next test case.
+    count += 1
+
+    # Run test case for everything.
+    JAVA_FILENAME = f"TrieTest{count}"
+
+    # Create the full filepath.
+    FILEPATH = TEST_DIR + JAVA_FILENAME
+
+    if (max_test is not None and count > max_test) or not os.path.exists(f"{FILEPATH}.java"):
+        break
+
+    # Compile java file.
+    os.system(f"javac {FILEPATH}.java")
+
+    # If the program did not successfully compile, there will not be a class file.
+    if not os.path.exists(f"{FILEPATH}.class"):
+        exit()
+
+    # If we make it here, the program compiled successfully.
+    # Get the number of test cases and run all of them. Default is 10 testcases.
+    TEST_CASES = 10 if argc < 3 or not argv[2].isdigit() else int(argv[2])
+
+    # Create empty dictionary of dictionaries.
+    thread_times = {version : {} for version in VERSIONS}
+
+    # For each test case, run the test then read the output file.
+    for i in range(TEST_CASES):
+        # Run testcase.
+        run = f"java {FILEPATH}"
+        for version in VERSIONS:
+            run += f" {version}"
+        
+        os.system(run)
+        
+        for version in VERSIONS:
+            # Open the file that the testcase created and read each line.
+            ifp = open(version + JSON_FILENAME)
+            for line in ifp:
+                # Split json key val pairs.
+                json_elements = line.split(',')
+
+                # Read first key val pair in json.
+                colon = json_elements[0].index(':')
+                num_threads = int(json_elements[0][colon+2:])
+
+                # Read second key val pair in json.
+                colon = json_elements[1].index(':')
+                time = int(json_elements[1][colon+1:])
+
+                # Put time in dictionary.
+                if num_threads in thread_times:
+                    thread_times[version][num_threads] = thread_times[num_threads] + time
+                else:
+                    thread_times[version][num_threads] = time
+            
+            os.system(f"rm {version + JSON_FILENAME}")
+
+    # print("--------------   Averages   --------------")
+    # print(thread_times)
+
+    for version in VERSIONS:
+        # Get all the averages for thread times.
+        for key in thread_times[version]:
+            thread_times[version][key] /= TEST_CASES
+
+
+    # Clean up the mess I made.
+    os.system(f"rm {TEST_DIR}*.class")
+    os.system(f"rm {TRIES_DIR}*.class")
+    os.system(f"rm {TEST_DIR}/all_tests/*.class")
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111)
+
+    ax.set_xlabel("Number of Threads")
+    ax.set_ylabel("Runtime (milliseconds)")
+
+    plt.xticks(np.arange(
+                start=0, 
+                stop=thread_times[VERSIONS[0]][max(thread_times[VERSIONS[0]], key=int)] + 1, 
+                step=5
+    ))
+
+    for version in VERSIONS:
+        x = np.array([key for key in thread_times[version]])
+        y = np.array([thread_times[version][key] for key in thread_times[version]])
+
+        ax.plot(x, y, label=version)
+
+    ax.legend()
+    fig.savefig("graph.png")
+    # plt.scatter(x, y) 
